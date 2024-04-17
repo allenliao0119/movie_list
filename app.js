c3c9278dfc8bb06a6cb5125e6ae651ed64806338
AllenLiao <allen10153@gmail.com>
2024-04-17 21:33:40 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.get('/', (req, res) => {
   res.render('index', {movies})
 })
 
+app.get('/movies/:id', (req, res) => {
+  const id = req.params.id
+  const movie = movies.find(movie => movie.id === Number(id))
+  res.render('show', {movie})
+})
+
 // start and listen on the server
 app.listen(port, () => {
   console.log(`Express is running on http:localhost:${port}`)
